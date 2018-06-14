a9738d23e74d79486906213d54c0aede73f966ae
Vladimir Milov <Vladimir_Milov@epam.com>
2018-06-14 15:23:35 +0300

    initial setup

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { VoicePlayer, VoiceRecognition } from 'react-voice-components';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <VoicePlayer
+          play
+          text="React voice player demonstration"
+        />,
+      </div>
+    );
+  }
+}
+
+export default App;
