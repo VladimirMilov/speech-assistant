8bb37b8690e5bab65ca2a25360caaf03b14e110f
Marin Aldimirov <Marin_Aldimirov@epam.com>
2018-06-14 18:07:00 +0200

    Update workflow

--- src/config/workflow.js
+++ src/config/workflow.js
@@ -1,33 +1,74 @@
-export const questionTypes = {
-    OPEN: 'QUESTION_TYPE_OPEN',
-    MULTI: 'MULTI',
-    RADIO: 'RADIO',
+const answerTypes = {
+    OPEN: 'ANSWER_TYPE_OPEN',
+    MULTI: 'ANSWER_TYPE_MULTI',
+    RADIO: 'ANSWER_TYPE_RADIO',
+    YESNO: 'ANSWER_TYPE_YESNO',
 }
 
-export const steps = [
+const steps = [
     {
         id: 0,
         component: 'Start',
-
+        keywords: {
+            'en-GB': ['start', 'begin'],
+            'nl-NL': ['begin', 'beginnen'],
+        },
     },
     {
         id: 1,
         component: 'Question',
-        type: this.questionTypes.OPEN,
-        title: 'Lorem ipsum dolor',
-        options: ['first', 'second', 'third']
+        answer: answerTypes.RADIO,
+        title: 'Multichoice question?',
+        options: [
+            {
+                label: 'Option 1',
+                keywords: ['one', '1'],
+                weight: 2.5,
+            },
+            {
+                label: 'Option 2',
+                keywords: ['two', '2'],
+                weight: 3.5,
+            },
+            {
+                label: 'Option 3',
+                keywords: ['three', '3'],
+                weight: 0.5,
+            },
+        ],
     },
     {
         id: 2,
         component: 'Question',
-        type: this.questionTypes.RADIO,
-        title: 'Lorem ipsum dolor',
-        options: ['first', 'second', 'third']
+        answer: answerTypes.YESNO,
+        title: 'Yes or no question?',
+        options: [
+            {
+                label: 'Yes',
+                keywords: ['yes', 'affirmative'],
+                weight: 250,
+            },
+            {
+                label: 'No',
+                keywords: ['no', 'denied'],
+                weight: 0,
+            },
+        ],
     },
     {
         id: 3,
-        component: 'Result',
+        component: 'Question',
+        type: answerTypes.OPEN,
         title: 'Lorem ipsum dolor',
-        ammount: 25730,
+    },
+    {
+        id: 4,
+        component: 'End',
+        title: 'Lorem ipsum dolor',
     },
 ]
+
+module.exports = {
+    steps,
+    answerTypes,
+}
